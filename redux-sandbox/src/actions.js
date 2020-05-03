cea37340fcbde618e57bc11b7b99084e6630a32f
Vitaly <vitalyzorich@gmail.com>
2020-05-03 15:56:40 +0300

    redux

---
+++ redux-sandbox/src/actions.js
@@ -0,0 +1,8 @@
+export const inc = () => ({type: 'INC'});
+
+export const dec = () => ({type: 'DEC'});
+
+export const rnd = () => ({
+	type: 'RND', 
+	payload: Math.floor(Math.random() * 10)
+});
---
+++ redux-sandbox/src/components/counter.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+const Counter = ({ counter, inc, dec, rnd }) => {
+	return (
+		<div className="jumbotron">
+			<h2>{counter}</h2>
+			<button 
+				className="btn btn-primary btn-lg"
+				onClick={inc} >INC</button>
+			<button 
+				className="btn btn-primary btn-lg"
+				onClick={dec} >DEC</button>
+			<button 
+				className="btn btn-primary btn-lg"
+				onClick={rnd} >RND</button>
+		</div>
+	)
+};
+
+const mapStateToProps = (state) => {
+	return {
+		counter: state
+	};
+};
+
+export default connect(mapStateToProps, actions)(Counter);
